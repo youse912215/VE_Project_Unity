a5084763cf87b8d0b8e7ef8d206161501196a0c2
YusukeMuto <74335173+youse912215@users.noreply.github.com>
2021-11-02 14:31:09 +0900

    Rename variable.

--- Assets/Script/ActVirus.cs
+++ Assets/Script/ActVirus.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.UI;
 
 using static Call.VirusData;
 using static Call.VirusData.VIRUS_NUM;
@@ -12,8 +13,8 @@ using static MouseCollision;
 
 public class ActVirus : MonoBehaviour
 {
-    private Virus[,] virus = new Virus[CATEGORY, OWNED]; //ウイルス構造体配列
-    private GameObject[,] virusObj = new GameObject[CATEGORY, OWNED]; //ウイルスオブジェクト配列
+    private VirusChildren[,] vChildren = new VirusChildren[CATEGORY, OWNED]; //ウイルス構造体配列
+    private GameObject[,] vObject = new GameObject[CATEGORY, OWNED]; //ウイルスオブジェクト配列
     private GameObject vPrefab; //ウイルスプレファブ
 
     private bool isGrabbedVirus; //ウイルスを掴んでいるか
@@ -23,16 +24,24 @@ public class ActVirus : MonoBehaviour
 
     private int storage;
 
+   
+    private VirusParents[] vParents = new VirusParents[CATEGORY];
+    
+
     // Start is called before the first frame update
     void Start()
     {
         /* ウイルス情報の初期化 */
-        InitValue(virus, CODE_CLD);
-        InitValue(virus, CODE_INF);
-        InitValue(virus, CODE_19);
+        InitValue(vParents, vChildren, CODE_CLD);
+        InitValue(vParents, vChildren, CODE_INF);
+        InitValue(vParents, vChildren, CODE_19);
         
         isGrabbedVirus = false;
         buttonMode = 0;
+
+        for (int i = 0; i < CATEGORY; ++i)
+            vParents[i].tag = VirusTagName[i];
+        vParents[0].creationCount = 5;
     }
 
     // Update is called once per frame
@@ -40,8 +49,11 @@ public class ActVirus : MonoBehaviour
     {
         worldPos = ReturnOnScreenMousePos(); //スクリーン→ワールド変換 //ウイルスを移動
 
-        Debug.Log("ウイルス番号::" + storage);
-        Debug.Log("Flag" + isGrabbedVirus);
+        vParents[0].obj = GameObject.FindGameObjectsWithTag(vParents[0].tag);
+        vParents[0].setCount = vParents[0].obj.Length - 1;
+        //Debug.Log("ウイルス数::" + vParents[0].setCount);
+        Debug.Log("storage::" + storage);
+
 
         if (!isGrabbedVirus && isMouseCollider && Input.GetMouseButtonDown(1)){
             OpenAfterMenu(); //設置後メニューを開く
@@ -49,8 +61,9 @@ public class ActVirus : MonoBehaviour
             SearchVirusArray();
         }
 
-        if (virus[buttonMode, vNum[buttonMode]].isActivity)
-            virusObj[buttonMode, vNum[buttonMode]].transform.position = worldPos; //ウイルスオブジェクトの位置をワールド座標で更新する
+        if (vParents[buttonMode].setCount == 0) return;
+        if (vChildren[buttonMode, /*vNum[buttonMode]*/vParents[buttonMode].setCount - 1].isActivity)
+            vObject[buttonMode, /*vNum[buttonMode]*/vParents[buttonMode].setCount - 1].transform.position = worldPos; //ウイルスオブジェクトの位置をワールド座標で更新する
         if (!isGrabbedVirus) return;
         OpenVirusMenu(); //ウイルスメニューを開く
     }
@@ -63,7 +76,7 @@ public class ActVirus : MonoBehaviour
     {
         if (isOpenMenu) return; //メニュー開いているときはスキップ
         if (isGrabbedVirus && buttonMode != n) return; //ウイルスを持っているときかつ、別のボタンの状態のときはスキップ
-        if (ProcessOutOfRange(n)) return; //例外処理ならスキップ
+        if (isLimitCapacity[n]) return; //例外処理ならスキップ
 
         isGrabbedVirus = ReverseFlag(isGrabbedVirus); //フラグを反転して、更新する
 
@@ -88,7 +101,7 @@ public class ActVirus : MonoBehaviour
     public void BackButtonPush()
     {
         //if (!isGrabbedVirus) return;
-        virus[buttonMode, vNum[buttonMode]].isActivity = true; //再びアクティブ状態に
+        vChildren[buttonMode, /*vNum[buttonMode]*/vParents[buttonMode].setCount - 1].isActivity = true; //再びアクティブ状態に
         ReverseMenuFlag(BACK); //メニューを閉じる
         isOpenMenu = false;
     }
@@ -104,7 +117,15 @@ public class ActVirus : MonoBehaviour
             DestroyBeforeVirus(); // ウイルスを削除する
             isGrabbedVirus = ReverseFlag(isGrabbedVirus);
         }
-        else Destroy(virusObj[storage / OWNED, storage % OWNED]);
+        else
+        {
+            int column = storage / OWNED;
+            int row = storage % OWNED;
+            if (vParents[column].setCount == vParents[column].creationCount)
+                isLimitCapacity[column] = false;
+            Debug.Log("aaa:::" + vObject[column, row].activeInHierarchy);
+            Destroy(vObject[column, row]); 
+        }
 
         ReverseMenuFlag(BACK); //メニューを閉じる
         isOpenMenu = false;
@@ -116,10 +137,10 @@ public class ActVirus : MonoBehaviour
     /// <param name="n">ウイルスの種類（番号）</param>
     public void CreateVirus(int n)
     {
-        GenerationVirus(virus, (VIRUS_NUM)n); //ウイルス生成
-        vPrefab = GameObject.Find(VirusName + n.ToString()); //ウイルス番号に合致するPrefabを取得
-        virusObj[n, vNum[n]] = Instantiate(vPrefab); //ゲームオブジェクトを生成
-        virusObj[n, vNum[n]].SetActive(true); //アクティブ状態にする
+        GenerationVirus(vParents, vChildren, (VIRUS_NUM)n); //ウイルス生成
+        vPrefab = GameObject.Find(VirusHeadName + n.ToString()); //ウイルス番号に合致するPrefabを取得
+        vObject[n, /*vNum[n]*/vParents[n].setCount] = Instantiate(vPrefab); //ゲームオブジェクトを生成
+        vObject[n, /*vNum[n]*/vParents[n].setCount].SetActive(true); //アクティブ状態にする
         buttonMode = n; //現在のボタンの状態を更新
     }
 
@@ -128,8 +149,8 @@ public class ActVirus : MonoBehaviour
     /// </summary>
     private void DestroyBeforeVirus()
     {
-        virus[buttonMode, vNum[buttonMode]].isActivity = false; //生存状態をfalse
-        Destroy(virusObj[buttonMode, vNum[buttonMode]]); //ゲームオブジェクトを削除
+        //vChildren[buttonMode, vNum[buttonMode]].isActivity = false; //生存状態をfalse
+        Destroy(vObject[buttonMode, /*vNum[buttonMode]*/vParents[buttonMode].setCount - 1]); //ゲームオブジェクトを削除
     }
 
     /// <summary>
@@ -137,7 +158,7 @@ public class ActVirus : MonoBehaviour
     /// </summary>
     private void SetVirus()
     {
-        GetVirus((VIRUS_NUM)buttonMode); //ウイルスをアクティブにする
+        GetVirus((VIRUS_NUM)buttonMode, vParents); //ウイルスをアクティブにする
         isGrabbedVirus = false; //ボタン操作をfalse
     }
 
@@ -152,14 +173,14 @@ public class ActVirus : MonoBehaviour
         if (Input.GetMouseButtonDown(1))
         {
             OpenBeforeMenu(); //メニューを開く
-            SaveVirusPosition(virus, (VIRUS_NUM)buttonMode, virusObj, worldPos); //設置したウイルス座標を保存
+            SaveVirusPosition(vParents, vChildren, (VIRUS_NUM)buttonMode, vObject, worldPos); //設置したウイルス座標を保存
             isOpenMenu = true;
         }
     }
 
     private void SearchVirusArray()
     {
-        Virus[] ary = virus.Cast<Virus>().ToArray(); //配列を一次元化
+        VirusChildren[] ary = vChildren.Cast<VirusChildren>().ToArray(); //配列を一次元化
         for (int i = 0; i < CATEGORY * OWNED; ++i)
         {
             //各ウイルスの座標と範囲オブジェクトの座標が一致するまで繰り返す
--- Assets/Script/VirusData.cs
+++ Assets/Script/VirusData.cs
@@ -7,7 +7,7 @@ namespace Call
 {
     public class VirusData : MonoBehaviour
     {
-        public struct Virus
+        public struct VirusChildren
         {
             public Vector3 pos; //位置
             public Quaternion rot; //角度
@@ -18,6 +18,14 @@ namespace Call
             public float durableValue; //耐久値
             public bool isActivity; //生存状態
             
+        }
+
+        public struct VirusParents
+        {
+            public GameObject[] obj; //親オブジェクト
+            public string tag; //タグ名
+            public int setCount; //現在設置した個数
+            public int creationCount; //作成した個数
         }
 
         //ウイルス番号
@@ -64,26 +72,27 @@ namespace Call
         public const int CATEGORY = 3; //ウイルスの種類
         public const int OWNED = 5; //ウイルスの保有数
 
-        public static int[] vNum = new int[CATEGORY]; //各ウイルスの設置数
+        //public static int[] vNum = new int[CATEGORY]; //各ウイルスの設置数
         public static bool[] isLimitCapacity = new bool[CATEGORY]; //限界容量
-        public static string VirusName = "Virus"; //ウイルス頭名
+        public static string VirusHeadName = "Virus"; //ウイルス頭名
+        public static string[] VirusTagName = {"Cold", "Inf", "19"};
 
         /// <summary>
         /// ウイルス番号に対応する、値をセットする
         /// </summary>
-        /// <param name="v">ウイルス構造体</param>
+        /// <param name="vC">ウイルス構造体</param>
         /// <param name="n">ウイルス名</param>
-        public static void InitValue(Virus[,] v, VIRUS_NUM n)
+        public static void InitValue(VirusParents[] vP, VirusChildren[,] vC, VIRUS_NUM n)
         {
             int i = (int)n; //番号取得
-            vNum[i] = 0; //保有数取得
+            vP[i].setCount = 0; //現在数取得
 
             for (int a = 0; a < OWNED; a++) {
-                v[i, a].pos = pos[i]; //位置取得
-                v[i, a].rot = rot[i]; //角度取得
-                v[i, a].scl = scl[i]; //スケール取得
-                v[i, a].force = force[i]; //3力取得
-                v[i, a].isActivity = false; //生存状態取得
+                vC[i, a].pos = pos[i]; //位置取得
+                vC[i, a].rot = rot[i]; //角度取得
+                vC[i, a].scl = scl[i]; //スケール取得
+                vC[i, a].force = force[i]; //3力取得
+                vC[i, a].isActivity = false; //生存状態取得
             }
         }
         
@@ -91,41 +100,43 @@ namespace Call
         /// ウイルスをアクティブにする
         /// </summary>
         /// <param name="n">ウイルス名</param>
-        public static void GetVirus(VIRUS_NUM n)
+        public static void GetVirus(VIRUS_NUM n, VirusParents[] vP)
         {
             int i = (int)n; //ウイルス番号
 
             //ウイルス内識別番号が保有数未満なら
-            if (vNum[i] < OWNED - 1) vNum[i]++;
-            else isLimitCapacity[i] = true; //限界容量に到達
+            //if (vP[i].setCount < vP[i].creationCount) vP[i].setCount++;
+            //else isLimitCapacity[i] = true; //限界容量に到達
+            if (vP[i].setCount == vP[i].creationCount) isLimitCapacity[i] = true; //限界容量に到達
         }
 
         /// <summary>
         /// ウイルスを生成し、生存情報を取得する
         /// </summary>
-        /// <param name="v">ウイルス構造体</param>
+        /// <param name="vC">ウイルス構造体</param>
         /// <param name="n">ウイルス名</param>
-        public static void GenerationVirus(Virus[,] v, VIRUS_NUM n)
+        public static void GenerationVirus(VirusParents[] vP,　VirusChildren[,] vC, VIRUS_NUM n)
         {
             int i = (int)n; //ウイルス番号
-            int j = vNum[i]; //ウイルス内識別番号
-            v[i, j].isActivity = true; //生存状態をアクティブ
+            int j = vP[i].setCount; //ウイルス内識別番号
+            vC[i, j].isActivity = true; //生存状態をアクティブ
         }
 
         /// <summary>
         /// 設置指定したウイルスの座標を保存する
         /// </summary>
-        /// <param name="v">ウイルス構造体</param>
+        /// <param name="vC">ウイルス構造体</param>
         /// <param name="n">ウイルス名</param>
         /// <param name="obj">ウイルスオブジェクト（プレハブ）</param>
         /// <param name="wPos">ワールド座標</param>
-        public static void SaveVirusPosition(Virus[,] v, VIRUS_NUM n, GameObject[,] obj, Vector3 wPos)
+        public static void SaveVirusPosition(VirusParents[] vP, VirusChildren[,] vC, VIRUS_NUM n,
+            GameObject[,] obj, Vector3 wPos)
         {
             int i = (int)n; //ウイルス番号
-            int j = vNum[i]; //ウイルス内識別番号
+            int j = vP[i].setCount - 1; //ウイルス内識別番号
             obj[i, j].transform.position = wPos; //ウイルスオブジェクトに、現在のマウスのワールド座標を保存
-            v[i, j].pos = wPos;  //ウイルス構造体に、現在のマウスのワールド座標を保存
-            v[i, j].isActivity = false; //生存状態をインアクティブ
+            vC[i, j].pos = wPos;  //ウイルス構造体に、現在のマウスのワールド座標を保存
+            vC[i, j].isActivity = false; //生存状態をインアクティブ
         }
 
         /// <summary>
