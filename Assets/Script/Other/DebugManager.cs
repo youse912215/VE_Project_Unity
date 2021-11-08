1f84a496e90e53660fc6718f5216a247adffc53e
YusukeMuto <74335173+youse912215@users.noreply.github.com>
2021-11-08 13:24:10 +0900

    Add debug function.

--- Assets/Script/Other/DebugManager.cs
+++ Assets/Script/Other/DebugManager.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class DebugManager : MonoBehaviour
 {
-
+    /// <summary>
+    /// デバッグ用
+    /// </summary>
+    /// <typeparam name="T">型</typeparam>
+    /// <param name="value">値</param>
+    public static void DebugFunc<T>(T value)
+    {
+        Debug.Log("{ " + value.GetType() + " }:" + value);
+    }
 }
