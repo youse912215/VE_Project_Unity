41297fac5b68ffacbdf4a3a2f7079c32700558bb
YusukeMuto <74335173+youse912215@users.noreply.github.com>
2021-11-08 15:19:15 +0900

    Changing material manager.

--- Assets/Script/UI/MaterialManager.cs
+++ Assets/Script/UI/MaterialManager.cs
@@ -1,21 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class MaterialManager : MonoBehaviour
 {
-    public Material normalMtl; //ノーマルマテリアル
-    public Material activeMtl; //アクション時マテリアル
-    public Material collisionMtl; //衝突時マテリアル
-
-    public static Material[] mat = new Material[3];
+    public static Material[] rangeMat = new Material[3]; //範囲マテリアル配列
+    public static Material[] createMat = new Material[3]; //作成マテリアル配列
 
     // Start is called before the first frame update
     void Start()
     {
-        mat[0] = normalMtl;
-        mat[1] = activeMtl;
-        mat[2] = collisionMtl;
+        //マテリアルを取得
+        for (int i = 0; i < rangeMat.Length; ++i)
+            rangeMat[i] = Resources.Load<Material>("Material/Range/Range" + i.ToString());
+        for (int i = 0; i < createMat.Length; ++i)
+            createMat[i] = Resources.Load<Material>("Material/Create/Create" + i.ToString());
     }
 
     // Update is called once per frame
