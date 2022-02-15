f867a4be4d695226bc6bb2205db309f59965c50a
YusukeMuto <74335173+youse912215@users.noreply.github.com>
2022-02-15 20:27:59 +0900

    Add warning event.

---
+++ Assets/Script/Enemy/StrongEnemyEvent.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class StrongEnemyEvent : MonoBehaviour
+{
+    [SerializeField] private Text message; //メッセージテキスト
+    private Image image; //格納用
+
+    //定数
+    private const int WAR_TIMES = 2; //回数
+    private const float INTERVAL = 1.0f; //間隔時間
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+        this.image.enabled = false;
+        StartCoroutine("Warning");
+    }
+
+    /// <summary>
+    /// UIの有効状態を切替
+    /// </summary>
+    /// <param name="state"></param>
+    private void ChangeEnabled(bool state)
+    {
+        this.image.enabled = state;
+        message.enabled = state;
+    }
+
+    /// <summary>
+    /// 警告するコルーチン
+    /// </summary>
+    IEnumerator Warning()
+    {
+        //初期化
+        GetComponent<AudioSource>().Play();
+        message.text = "強敵出現!";
+        var state = true;
+
+        //WAR_TIMES * 2回分回す
+        for (int i = 0; i < WAR_TIMES * 2; ++i)
+        {
+            ChangeEnabled(state); //状態を切替
+            yield return new WaitForSeconds(INTERVAL); //待機時間
+            state = !state; //bool反転
+        }
+
+        //終了処理
+        message.text = "";
+        GetComponent<AudioSource>().Stop();
+    }
+}
